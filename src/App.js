ad5ce7f7053dea5f34a21170c0a912c58d8ccbe1
Rodolfo Mori <rod_dolpho@hotmail.com>
2021-12-09 17:52:47 -0300

    finished application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import FormLogo from "./assets/form-logo.png";
+
+import "./App.css";
+
+const schema = yup
+  .object({
+    name: yup.string().required("O nome é obrigatório"),
+    email: yup
+      .string()
+      .email("Digite um email válido")
+      .required("O email é obrigatório"),
+    password: yup
+      .string()
+      .min(6, "A senha deve ter pelo menos 6 digitos")
+      .required("A senha é obrigatória"),
+    confirmPassword: yup
+      .string()
+      .required("Confirmar a senha é obrigatório")
+      .oneOf([yup.ref("password")], "As senhas devem ser iguais"),
+  })
+  .required();
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  console.log(watch("name")); 
+
+  function onSubmit(userData) {
+    console.log(userData);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <img src={FormLogo} alt="imagem-logo" />
+
+      <label>
+        Nome
+        <input type="text" {...register("name", { required: true })} />
+        <span>{errors.name?.message}</span>
+      </label>
+
+      <label>
+        Email
+        <input type="text" {...register("email")} />
+        <span>{errors.email?.message}</span>
+      </label>
+
+      <label>
+        Senha
+        <input type="password" {...register("password")} />
+        <span>{errors.password?.message}</span>
+      </label>
+
+      <label>
+        Confirmar Senha
+        <input type="password" {...register("confirmPassword")} />
+        <span>{errors.confirmPassword?.message}</span>
+      </label>
+
+      <button type="submit">Cadastrar-se</button>
+    </form>
   );
 }
 
